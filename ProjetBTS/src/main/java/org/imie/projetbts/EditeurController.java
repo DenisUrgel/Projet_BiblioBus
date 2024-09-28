b9384fab477fb9a80e0772a4980d64c7269d26af
Sakagnir71 <159887743+Sakagnir71@users.noreply.github.com>
2024-09-28 14:58:22 +0200

    Add files via upload

---
+++ ProjetBTS/src/main/java/org/imie/projetbts/EditeurController.java
@@ -0,0 +1,16 @@
+package org.imie.projetbts;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TextField;
+
+public class EditeurController {
+    public TableColumn colNomEditeur;
+    public TableColumn colIdEditeur;
+    public TextField txtNomEditeur;
+    public Button btnAddEditeur;
+
+    public void onAddEditeur(ActionEvent actionEvent) {
+    }
+}
